a4a617c62c23475b600aacca6f8c16ee8bc75e08
David <david@rlabs.org>
2023-06-19 18:29:48 +0200

    convert to async

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,38 +1,39 @@
 import json 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name #%s mean? https://www.geeksforgeeks.org/what-does-s-mean-in-a-python-format-string/
 
         #Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
-    ##
+    
     #Receive message from WebSocket
-    def receive(self,text_data):
+    async def receive(self,text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         isTyping = text_data_json['isTyping']
         username = text_data_json['username']
         clickedSend = text_data_json['clickedSend']
         #gets info and saves to channel layer group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,{'type': 'chat_message','message': message,'isTyping':isTyping,'username':username,'clickedSend':clickedSend}
         )
 
     #Receive message from group
-    def chat_message(self,event):
+    async def chat_message(self,event):
         type,message,isTyping,username,clickedSend = event['type'],event['message'],event['isTyping'],event['username'],event['clickedSend']
         if isTyping or clickedSend == False:
             if username != str(self.scope['user']):
@@ -44,7 +45,7 @@ class ChatConsumer(WebsocketConsumer):
                 message = ''
 
         #Send message to WebSocket
-        self.send(text_data = json.dumps({'message':message,'isTyping':isTyping,'clickedSend':clickedSend,'username':username}))
+        await self.send(text_data = json.dumps({'message':message,'isTyping':isTyping,'clickedSend':clickedSend,'username':username}))
     
 ###############################################
 ###########------PREVIOUS STEP------###########
