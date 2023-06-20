00cb41417596cd6bd6cfdff9f193b97d08f2ec6f
David <david@rlabs.org>
2023-06-20 20:58:37 +0200

    display previous messages

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Message
 
 def index(request):
     return render(request,'chat/index.html')
 
 def room(request,room_name):
-    return render(request,'chat/room.html',{'room_name':room_name})
+    messages = Message.objects.filter(room_name=room_name)
+    return render(request,'chat/room.html',{'room_name':room_name,'messages':messages})
